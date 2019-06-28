61707fd961f7f6ae79a17a215adf6802a555f08b
magical-flow-3186 <rregalado17@gmail.com>
2019-06-28 21:14:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog 
   
+  def intialize(name, breed)
+    @name = name
+    @breed = breed
+  end
+  
+  attr_accessor :name
+  
+  def breed=(breed)
+    @breed = breed
+  end
+  
+  def breed
+    @breed
+  end
 end
